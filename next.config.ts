2f4342e406fd4aed2869543f18884c2f850b3ec5
Riquelmi Santos <143042136+RiquelmiDev@users.noreply.github.com>
2025-07-17 01:15:53 -0300

    chore: add and config pwa for project

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,23 @@
-import type { NextConfig } from "next";
+import withPWA from 'next-pwa';
+import type { RemotePattern } from "next/dist/shared/lib/image-config";
 
-const nextConfig: NextConfig = {
+const remotePatterns: RemotePattern[] = [
+  {
+    protocol: "https",
+    hostname: "image.tmdb.org",
+    pathname: "/t/p/**",
+  },
+];
+
+const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'image.tmdb.org',
-        pathname: '/t/p/**', 
-      },
-    ],
+    remotePatterns,
   },
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+})(nextConfig);
